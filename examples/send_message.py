f5d918e3410d885defcb3fc3373022cc09e9243b
D. Rakyan Erlangga Rizki Wardhana <85488433+drakyanerlanggarizkiwardhana@users.noreply.github.com>
2023-04-25 23:16:02 +0700

    Add files via upload

---
+++ examples/send_message.py
@@ -0,0 +1,12 @@
+import poe
+import logging
+import sys
+
+#send a message and stream the response
+
+token = sys.argv[1]
+client = poe.Client(token)
+
+message = "Summarize the GNU GPL v3"
+for chunk in client.send_message("capybara", message, with_chat_break=True):
+  print(chunk["text_new"], end="", flush=True)
